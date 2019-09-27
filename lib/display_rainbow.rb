054f848d93edd660f3284e940b22f1f226d1a463
brandon moro <brandon_078@hotmail.com>
2019-09-27 00:09:57 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
+bob = ["Bob", 46, "Juggler", "555-555-555"]
+stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
 def print_business_card(person)
-  bob = ["Bob", 46, "Juggler", "555-555-555"]
   puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
-  stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
   puts "Name: #{stefani[0]}, Age: #{stefani[1]}, Occupation: #{stefani[2]}"
 end
